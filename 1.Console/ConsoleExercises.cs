6961b0418e435c57bc9e9c84ec15b6c5ef3844a5
bboycien <bboycien@hotmail.com>
2019-05-09 20:35:56 -0300

    Ejercicios básicos de csharp, lectura y escritura por teclado, tipos de variables.

--- 1.Console/ConsoleExercises.cs
+++ 1.Console/ConsoleExercises.cs
@@ -6,6 +6,16 @@ namespace CSharpExercises{
             Console.WriteLine("¿Cuántos años tenes?: ");
             string age = Console.ReadLine();
             Console.WriteLine("Wow, no pareces de " + age + " años");
+            Console.WriteLine("¿Cual es tu nombre?: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("¿Y tu apellido?: ");
+            string lastName = Console.ReadLine();
+             Console.WriteLine("¿cual es tu número de DNI?: ");
+             string DNI= Console.ReadLine();
+             Console.WriteLine("Según vos tu nombre es " + name + ",tu apellido es "+ lastName +" y tu dni es "+DNI+".");
+
+
+
             Console.ReadLine();
         }
     }
