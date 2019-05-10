b68c55a0b6785bf756cf42070a222344dc0f8b78
bboycien <bboycien@hotmail.com>
2019-05-09 21:59:52 -0300

    Ejercicios con condicionales (if), falta realizar el selector de programa a ejecutar

--- 3.TiposDatos/DataTypeExercises.cs
+++ 3.TiposDatos/DataTypeExercises.cs
@@ -5,11 +5,24 @@ namespace CSharpExercises{
         public void ValueSum(){
             Console.WriteLine("Ingrese un valor a sumar:");
             var firstValue = Console.ReadLine();
-
             Console.WriteLine("Ingrese otro valor a sumar:");
             var secondValue = Console.ReadLine();
-
-            Console.WriteLine("El resultado de la suma es: {0}", firstValue + secondValue);
+            int first=0;
+            int second=0;
+            bool firstNumber = Int32.TryParse (firstValue, out first );
+            bool secondNumber = Int32.TryParse (secondValue, out second );
+            
+                if(firstNumber && secondNumber){
+                    Console.WriteLine("El resultado de la suma es: {0}", first + second);
+            }
+            else if (firstNumber){
+                Console.WriteLine("El primer parametro no es entero");
+            }
+            else if (secondNumber) {
+                Console.WriteLine("El segundo parametro no es entero");
+            }
+           
+            
         }
     }
 }
